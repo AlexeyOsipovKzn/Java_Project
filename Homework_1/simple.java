0de49736b1f9119a4370be858057867e5c98fa31
Alexey_Osipov <osipov-leha@inbox.ru>
2022-12-01 20:30:32 +0400

    simple

---
+++ Homework_1/simple.java
@@ -0,0 +1,10 @@
+package Java_Project.Homework_1;
+
+public class simple {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
+        for (int i = 1; i < 1001; i++) {
+            System.out.println(i);
+        }
+    }
+}
